f2321a03610967ce66271b41b8a1da22cc8bcada
Illia Piddubnyi <illya2008zp@gmail.com>
2025-02-13 19:14:05 +0200

    Add basic logout functionality

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -22,6 +22,10 @@ export async function login(userData) {
   return user;
 }
 
+export const logout = () => {
+  window.localStorage.removeItem("refresh");
+}
+
 export async function refreshSession(userId) {
   const {
     data: { data: user },
--- client/src/components/AuthNav/index.jsx
+++ client/src/components/AuthNav/index.jsx
@@ -1,11 +1,20 @@
 import { NavLink, useLocation } from "react-router";
 import styles from "./AuthNav.module.scss";
+import { useContext } from "react";
+import { UserContext } from "../../contexts";
+import { logout } from "../../api";
+
 const setActive = ({ isActive }) =>
   isActive ? styles.activeLink : styles.link;
+
 const AuthNav = () => {
+  const [user, setUser] = useContext(UserContext);
+  
   const location = useLocation();
+
   const isAuthPage = location.pathname.includes("auth");
   const isLoginPage = location.pathname.includes("login");
+
   const authLinks = (
     <NavLink
       to={`/auth/${isLoginPage ? "registration" : "login"}`}
@@ -14,6 +23,7 @@ const AuthNav = () => {
       {isLoginPage ? "Registration" : "Login"}
     </NavLink>
   );
+
   const guestLinks = (
     <>
       <NavLink to="/auth/login" className={setActive}>
@@ -24,9 +34,15 @@ const AuthNav = () => {
       </NavLink>
     </>
   );
+
+  const logoutBtn = <button onClick={() => {
+    logout();
+    setUser(null);
+  }}>Logout</button>
+
   return (
     <div className={styles.container}>
-      {isAuthPage ? authLinks : guestLinks}
+      {user ? logoutBtn : isAuthPage ? authLinks : guestLinks}
     </div>
   );
 };
