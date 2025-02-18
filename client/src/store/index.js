91d6be0d6213932e15c8375b0ca5d17844f955f3
Illia Piddubnyi <illya2008zp@gmail.com>
2025-02-18 20:36:16 +0200

    Add redux devtools to project

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,7 +1,8 @@
 import { legacy_createStore as createStore } from "redux";
 import counterReducer from "./reducers/counterReducer";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 // створення стори станів редаксу
-const store = createStore(counterReducer);
+const store = createStore(counterReducer, composeWithDevTools());
 
 export default store;
