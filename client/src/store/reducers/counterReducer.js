70d18460eb199c0f1ab117d67f155f54550940ca
Illia Piddubnyi <illya2008zp@gmail.com>
2025-02-21 18:17:10 +0200

    Add createReducer example

--- client/src/store/reducers/counterReducer.js
+++ client/src/store/reducers/counterReducer.js
@@ -1,5 +1,5 @@
-import {increment, decrement, setStep} from "../actions/actionCreators";
-
+import { createReducer } from "@reduxjs/toolkit";
+import { increment, decrement, setStep } from "../actions/actionCreators";
 
 // початковий стан для редюсера
 const initialState = {
@@ -9,39 +9,62 @@ const initialState = {
 
 // базовий редаксівський редюсер
 // початковий стан передаємо як значення за замовчанням для state
-function counterReducer(state = initialState, action) {
-  // по типу екшену визначаємо його логіку
-  switch (action.type) {
-    case increment.type: {
-      // створюємо новий стан та повертаємо як результат редюсера
-      const newIncrementState = {
-        ...state,
-        count: state.count + state.step,
-      };
+// export default function counterReducer(state = initialState, action) {
+//   // по типу екшену визначаємо його логіку
+//   switch (action.type) {
+//     case increment.type: {
+//       // створюємо новий стан та повертаємо як результат редюсера
+//       const newIncrementState = {
+//         ...state,
+//         count: state.count + state.step,
+//       };
 
-      return newIncrementState;
-    }
-    case decrement.type: {
-      const newLesionState = {
-        ...state,
-        count: state.count - state.step,
-      };
+//       return newIncrementState;
+//     }
+//     case decrement.type: {
+//       const newLesionState = {
+//         ...state,
+//         count: state.count - state.step,
+//       };
 
-      return newLesionState;
-    }
-    case setStep.type: {
-      const newStep = isNaN(+action.payload) ? state.step : +action.payload;
-      const newStepState = {
-        ...state,
-        step: newStep,
-      };
+//       return newLesionState;
+//     }
+//     case setStep.type: {
+//       const newStep = isNaN(+action.payload) ? state.step : +action.payload;
+//       const newStepState = {
+//         ...state,
+//         step: newStep,
+//       };
 
-      return newStepState;
-    }
-    // якщо тип екшена невідомий або відсутній то повертаємо старий стан
-    default:
-      return state;
-  }
-}
+//       return newStepState;
+//     }
+//     // якщо тип екшена невідомий або відсутній то повертаємо старий стан
+//     default:
+//       return state;
+//   }
+// }
+
+/*
+  createReducer - утіліти для створення редюсера
+    приймає початковий стан та коллбек як аргументи
+*/
+const counterReducer = createReducer(initialState, (builder) => {
+  // кейс створюємо через builder.addCase, перший аргумент екшн тайп або екшн креатор створений через createAction
+  // 2 аргумент - мініредюсер в якому можна писати мутабельний код
+  builder.addCase(increment.type, (state, action) => {
+    // state.count = state.count + state.step;
+    state.count += state.step;
+  });
+
+  builder.addCase(decrement, (state) => {
+    state.count -= state.step;
+  });
+
+  builder.addCase(setStep, (state, action) => {
+    state.step = !isNaN(action.payload) ? +action.payload : state.step;
+  });
+
+  
+});
 
 export default counterReducer;
