f58108f90532100dcc11fbd3f42c2d83ffa317b7
Illia Piddubnyi <illya2008zp@gmail.com>
2025-03-02 18:13:29 +0200

    Refactor chat routers

--- server/routers/chatRouter.js
+++ server/routers/chatRouter.js
@@ -1,10 +1,15 @@
 const chatRouter = require("express").Router();
+const ChatMessageController = require("../controllers/chatMessageController")
 const ChatController = require("../controllers/chatController");
 
 chatRouter.post("/", ChatController.createChat);
 
-chatRouter.post("/addUser", ChatController.addUserToChat);
+chatRouter.post("/:chatId/", ChatController.addUserToChat);
 
-chatRouter.get("/:userId/chats", ChatController.getUserChats);
+chatRouter.get("/users/", ChatController.getUserChats);
+
+chatRouter.post("/:chatId/messages", ChatMessageController.createMessage);
+
+chatRouter.get("/:chatId/messages", ChatMessageController.getChatMessages);
 
 module.exports = chatRouter;
--- server/routers/index.js
+++ server/routers/index.js
@@ -10,7 +10,6 @@ rootRouter.use("/auth", authRouter);
 rootRouter.use("/users", chatRouter);
 
 rootRouter.use("/chats", chatRouter);
-rootRouter.use("/messages", chatMessageRouter);
 
 rootRouter.get("/secret", checkAccessToken, (req, res, next) => {
   res.status(200).send({data: "secret data"})
